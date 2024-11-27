c975d6d4b1266d1f0254e8014019cb316414fd4d
jakeymac <jakeymacaroni@gmail.com>
2024-11-27 09:29:18 -0700

    add-workflows-page-step-complete

--- tethysapp-workflows_tutorial/tethysapp/workflows_tutorial/controllers.py
+++ tethysapp-workflows_tutorial/tethysapp/workflows_tutorial/controllers.py
@@ -1,75 +1,14 @@
 from tethys_sdk.routing import controller
-from tethys_sdk.gizmos import Button
+from tethysext.workflows.views.layouts import WorkflowLayout
 from .app import App
+from .map_manager import MapManager
+from .spatial_manager import SpatialManager
 
 
-@controller
-def home(request):
-    """
-    Controller for the app home page.
-    """
-    save_button = Button(
-        display_text='',
-        name='save-button',
-        icon='save',
-        style='success',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Save'
-        }
-    )
-
-    edit_button = Button(
-        display_text='',
-        name='edit-button',
-        icon='pen',
-        style='warning',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Edit'
-        }
-    )
-
-    remove_button = Button(
-        display_text='',
-        name='remove-button',
-        icon='trash',
-        style='danger',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Remove'
-        }
-    )
-
-    previous_button = Button(
-        display_text='Previous',
-        name='previous-button',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Previous'
-        }
-    )
-
-    next_button = Button(
-        display_text='Next',
-        name='next-button',
-        attributes={
-            'data-bs-toggle': 'tooltip',
-            'data-bs-placement': 'top',
-            'title': 'Next'
-        }
-    )
-
-    context = {
-        'save_button': save_button,
-        'edit_button': edit_button,
-        'remove_button': remove_button,
-        'previous_button': previous_button,
-        'next_button': next_button
-    }
-
-    return App.render(request, 'home.html', context)
+@controller(name="home", url="home")
+class WorkflowLayoutController(WorkflowLayout):
+    app = App
+    base_template = 'workflows_tutorial/base.html'
+    
+    def __init__(self):
+        super().__init__(SpatialManager, MapManager, App.DATABASE_NAME)
