db1a3ea137c6493a2d7b92066590d000291aea17
jakeymac <jakeymacaroni@gmail.com>
2024-12-04 17:39:34 -0700

    added model.py

---
+++ tethysapp-workflows_tutorial/tethysapp/workflows_tutorial/model.py
@@ -0,0 +1,10 @@
+from tethysext.workflows.models.base import WorkflowsBase
+
+def init_db(engine, first_time):
+    """
+    An example persistent store initializer function
+    """
+    # Create tables
+    WorkflowsBase.metadata.create_all(engine)
+
+    
