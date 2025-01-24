6e66ec6adb5dc3212cd239a8d746530d7ed1809c
jakeymac <jakeymacaroni@gmail.com>
2025-01-24 14:45:21 -0700

    Added description to basic workflow to accomodate update to workflows extension

--- tethysapp-workflows_tutorial/tethysapp/workflows_tutorial/workflows/basic_workflow/__init__.py
+++ tethysapp-workflows_tutorial/tethysapp/workflows_tutorial/workflows/basic_workflow/__init__.py
@@ -14,14 +14,15 @@ class BasicWorkflow(WorkflowBase):
     __mapper_args__ = {'polymorphic_identity': TYPE}
 
     @classmethod
-    def new(cls, app, name, creator_id, creator_name, geoserver_name, map_manager, spatial_manager, **kwargs):
+    def new(cls, app, name, creator_id, description, creator_name, geoserver_name, map_manager, spatial_manager, **kwargs):
         """
         Factor class method that creates a new workflow with steps
         Args:
             app(TethysApp): The TethysApp hosting this workflow (e.g. Agwa).
             name(str): Name for this instance of the workflow.
-            resource_id(str|uuid): ID of the resource.
             creator_id(str): Username of the user that created the workflow.
+            description(str): Description of the workflow.
+            creator_name(str): Username of the creator of the workflow.
             geoserver_name(str): Name of the SpatialDatasetServiceSetting pointing at the GeoServer to use for steps with MapViews.
             map_manager(MapManagerBase): The MapManager to use for the steps with MapViews.
             spatial_manager(SpatialManager): The SpatialManager to use for the steps with MapViews.
@@ -31,7 +32,7 @@ class BasicWorkflow(WorkflowBase):
             Workflow: the new workflow.
         """
         # Create new workflow instance
-        workflow = cls(name=name, creator_id=creator_id, creator_name=creator_name)
+        workflow = cls(name=name, description=description, creator_id=creator_id, creator_name=creator_name)
 
         boundary_step = SpatialInputStep(
             name='Boundary Input Step',
